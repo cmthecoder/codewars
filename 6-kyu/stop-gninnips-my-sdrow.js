556a1fa53dd10ffe86e1a68b60e5a03bbb4ddc37
Chris Mems <christopher.mems@.com>
2023-02-21 10:08:28 -0600

    solve reverse string of a certain length challenge

---
+++ 6-kyu/stop-gninnips-my-sdrow.js
@@ -0,0 +1,21 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+
+function spinWords(str){
+  let arr = []
+  str = str.split(' ')
+  str.forEach(word => {
+    if(word.length >= 5){
+      arr.push(word.split('').reverse().join(''))
+    } else {
+      arr.push(word)
+    }
+  })
+  return arr.join(' ')
+}
