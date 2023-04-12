25ef4ab0aea90fef11bfc39b5a104081328f107b
Chris Mems <christopher.mems@.com>
2023-04-12 09:13:48 -0500

    include other ways to solve isograms challenge

--- 7-kyu/isograms.js
+++ 7-kyu/isograms.js
@@ -14,3 +14,23 @@ function isIsogram(str){
   str = str.toLowerCase()
   return new Set(str).size === str.length
 }
+
+// Other ways to solve it
+
+function isIsogram(str){
+	return new Set(str.toUpperCase()).size == str.length;
+}
+
+function isIsogram(str){
+  var i, j;
+  str = str.toLowerCase();
+  for(i = 0; i < str.length; ++i)
+    for(j = i + 1; j < str.length; ++j)
+      if(str[i] === str[j])
+        return false;
+  return true;
+}
+
+function isIsogram(str){
+  return !str.match(/([a-z]).*\1/i);
+}
